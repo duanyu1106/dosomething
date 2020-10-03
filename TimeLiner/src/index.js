67fa7f0534960ddae3d5588ca8f10d6f7c493973
duanyu <duanyu1106@outlook.com>
2020-10-03 12:02:40 +0800

    feat: init TimeLiner.js

---
+++ TimeLiner/src/index.js
@@ -0,0 +1,4 @@
+import TimeLiner from "./TimeLiner";
+
+let wrap = document.getElementById('wrap')
+new TimeLiner(wrap)
---
+++ TimeLiner/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'eval-cheap-module-source-map',
+  entry: {
+    main: './src/index.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    open: true,
+    port: 8080
+  },
+  module: {
+    rules: [{
+      test: /\.(jpg|png|gif)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          name: '[name]_[hash].[ext]',
+          outputPath: 'images/',
+          limit: 10240
+        }
+      }
+    }, {
+      test: /\.(eot|ttf|svg)$/,
+      use: {
+        loader: 'file-loader'
+      }
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    globalObject: 'this'
+  }
+}
